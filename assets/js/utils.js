368bf77987d4162eb3d04ebbf87fc4fcca4ef47e
TroJan-88888 <boonchai888888882@gmail.com>
2025-12-02 14:14:32 +0700

    Create utils.js

---
+++ assets/js/utils.js
@@ -0,0 +1,3 @@
+function toggleMode(){document.body.classList.toggle('light');}
+function hideLoading(){document.getElementById('loading')?.style.display='none';}
+function showLoading(){document.getElementById('loading')?.style.display='flex';}
